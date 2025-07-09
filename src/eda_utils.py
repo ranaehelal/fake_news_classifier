0f70cec85f38fd47e7b1017401dab87240deb254
rana-helal <res.maika@gmail.com>
2025-07-09 18:36:25 +0300

    add functions for plotting top words and extracting top N words from text data

--- src/eda_utils.py
+++ src/eda_utils.py
@@ -1,6 +1,8 @@
 import os
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
 from wordcloud import WordCloud
+import seaborn as sns
 
 def generate_wordcloud(df, label, source=None, base_dir="visualizations/wordclouds"):
     """
@@ -27,3 +29,32 @@ def generate_wordcloud(df, label, source=None, base_dir="visualizations/wordclou
     save_path = os.path.join(save_dir, f"{label}_wc.png")
     plt.savefig(save_path)
     plt.close()
+
+def plot_top_words (word_df,x_label ='Frequency', y_label='Words', title='Top Words'):
+    word_df.columns = ['word', 'freq']
+
+    plt.figure(figsize=(10, 6))
+
+    # Create the plot
+    ax = sns.barplot(
+        data=word_df,
+        x='freq',
+        y='word',
+
+    )
+
+
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.tight_layout()
+
+    plt.show()
+
+
+def get_top_n_words(data, n=None):
+    vec = CountVectorizer(stop_words='english').fit(data)
+    bag = vec.transform(data)
+    sum_words = bag.sum(axis=0)
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    return sorted(words_freq, key=lambda x: x[1], reverse=True)[:n]
